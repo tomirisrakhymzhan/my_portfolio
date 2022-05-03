9d6a0df71259343e611824c855a34509d272c36a
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-03 19:37:30 +0100

    Header component base structure

--- frontend_myportfolio/src/containers/Header/Header.jsx
+++ frontend_myportfolio/src/containers/Header/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './Header.scss';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 3  }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Tomiris</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Software Engineer</p>
+            <p className="p-text">.NET Developer</p>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+
   );
 }
 
