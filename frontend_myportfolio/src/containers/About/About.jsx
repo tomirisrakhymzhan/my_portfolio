eb92c5a753973faae0d09174d27b43437539ee2f
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-05-04 18:22:09 +0100

    connected sanity content studio to change website contents

--- frontend_myportfolio/src/containers/About/About.jsx
+++ frontend_myportfolio/src/containers/About/About.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './About.scss';
+import { urlFor, client } from '../../client';
+
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
-const abouts = [
-  {title: 'Frontend Development', description: 'I love mastering my frontend skills with HTML, CSS (Sass), Javscript (with React framework) in order to make the smoothest and responsive user experience.', imgUrl: images.about05},
-  {title: 'Backend Development', description: 'I have strong foundation in C# and a good grasp of the highly efficient server-side framework ASP.NET using MVC paradigm. I care a lot about making readable, maintainable and efficient code, and I love expanding my knowledge.', imgUrl: images.about04},
-];
+// const abouts = [
+//   {title: 'Frontend Development', description: 'I love mastering my frontend skills with HTML, CSS (Sass), Javscript (with React framework) in order to make the smoothest and responsive user experience.', imgUrl: images.about05},
+//   {title: 'Backend Development', description: 'I have strong foundation in C# and a good grasp of the highly efficient server-side framework ASP.NET using MVC paradigm. I care a lot about making readable, maintainable and efficient code, and I love expanding my knowledge.', imgUrl: images.about04},
+// ];
+
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">I am a <span>Quick learner</span> <br />and a <span>Problem Solver</span></h2>
@@ -19,7 +31,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
